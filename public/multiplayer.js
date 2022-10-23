22a453970acddbe1e8811ee339b6d68d877c5bd2
Satvik Borra <borrasatvik@gmail.com>
2022-10-22 21:08:32 -0400

    online boilerplate

---
+++ public/multiplayer.js
@@ -0,0 +1,82 @@
+const socket = io('/');
+const peer = new Peer();
+let myVideoStream;
+let myId;
+var videoGrid = document.getElementById('videoDiv')
+var myvideo = document.createElement('video');
+myvideo.muted = true;
+const peerConnections = {}
+navigator.mediaDevices.getUserMedia({
+  video:true,
+  audio:true
+}).then((stream)=>{
+  myVideoStream = stream;
+  addVideo(myvideo , stream);
+  peer.on('call' , call=>{
+    call.answer(stream);
+    
+    const vid = document.createElement('video');
+    call.on('stream' , userStream=>{
+      addVideo(vid , userStream);
+    })
+    call.on('error' , (err)=>{
+      alert(err)
+    })
+    call.on("close", () => {
+        console.log(vid);
+        vid.remove();
+    })
+    peerConnections[call.peer] = call;
+  })
+}).catch(err=>{
+    alert(err.message)
+})
+
+peer.on('error' , (err)=>{
+  alert(err.type);
+});
+
+socket.on('userDisconnect' , id=>{
+  if(peerConnections[id]){
+    peerConnections[id].close();
+  }
+})
+function addVideo(video , stream){
+  video.srcObject = stream;
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.append(video);
+}
+peer.on('open' , (id)=>{
+  myId = id;
+})
+
+
+function createRoom(){
+
+}
+
+function joinRoom(){
+  var roomCode = document.getElementById("roomCodeInput").value;
+  console.log("join "+roomCode)
+  socket.emit("newUser" , myId , roomCode);
+
+ 
+  socket.on('userJoined' , id=>{
+    console.log("new user joined")
+    const call  = peer.call(id , myVideoStream);
+    const vid = document.createElement('video');
+    call.on('error' , (err)=>{
+      alert(err);
+    })
+    call.on('stream' , userStream=>{
+      addVideo(vid , userStream);
+    })
+    call.on('close' , ()=>{
+      vid.remove();
+      console.log("user disconect")
+    })
+    peerConnections[id] = call;
+  })
+}
